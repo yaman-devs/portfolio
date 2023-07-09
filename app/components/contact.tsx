d99d6e969b4a615f05ebffafb97242422d276c70
Quick-rev <quickreviews.b+github@gmail.com>
2023-07-09 12:07:15 +0300

    a

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -2,45 +2,54 @@
 
 export default function Contact() {
   return (
-    <form
-      method="POST"
-      data-netlify="true"
-      action={"/"}
-      name="contact"
-      className="flex flex-col justify-center gap-2 items-center min-h-[250px] bg-primary-button"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <div>
-        <input
-          className="min-w-[300px] px-2 bg-transparent border focus-visible:outline-none "
-          type="text"
-          name="name"
-          placeholder="Name"
-        />
+    <div className="flex flex-col justify-center items-center bg-primary-button py-8 ">
+      <h1 className="text-2xl md:text-3xl mb-8">Contact Me</h1>
+      <div className="px-10 text-center md:px-32  md:max-w-[60rem] mb-8 md:mb-16">
+        I am currently seeking new opportunities.
+        <br /> If you are interested in collaborating or have any inquiries,
+        please feel free to send me a message, and I will promptly respond to
+        you. Thank you!
       </div>
-      <div>
-        <input
-          type="text"
-          name="Email"
-          placeholder="Email"
-          className="min-w-[300px] px-2 bg-transparent border focus-visible:outline-none "
-        />
-      </div>
-      <div>
-        <textarea
-          name="message"
-          placeholder="Message"
-          className="min-w-[300px] min-h-[100px] px-2 bg-transparent border focus-visible:outline-none "
-        />
-      </div>
-      <div>
-        <button
-          type="submit"
-          className="px-2 mt-4 bg-transparent border focus-visible:outline-none hover:opacity-75 "
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+      <form
+        method="POST"
+        data-netlify="true"
+        action={"/"}
+        name="contact"
+        className="flex flex-col w-full justify-center gap-2 items-center min-h-[250px] "
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <div>
+          <input
+            className="min-w-[300px] lg:min-w-[500px] px-2 py-1 bg-transparent border focus-visible:outline-none "
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="Email"
+            placeholder="Email"
+            className="min-w-[300px] lg:min-w-[500px] px-2 py-1 bg-transparent border focus-visible:outline-none "
+          />
+        </div>
+        <div>
+          <textarea
+            name="message"
+            placeholder="Message"
+            className="min-w-[300px] lg:min-w-[500px] min-h-[100px] px-2 py-1 bg-transparent border focus-visible:outline-none "
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="px-2 mt-4 bg-transparent border focus-visible:outline-none hover:opacity-75 "
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <main className="w-screen">
+    <main>
       <Hero />
     </main>
   );
